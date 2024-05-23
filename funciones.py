a2f6aeed09db133c68ef2e836de3f08be916c6f9
LuciaEscobedo <101607876+LuciaEscobedo@users.noreply.github.com>
2024-05-23 00:43:58 -0300

    Funcion UserData

--- funciones.py
+++ funciones.py
@@ -6,6 +6,7 @@ import pandas as pd
 # Cargar el Dataset
 df_desarrolladores = pd.read_parquet('./Datasets/def_developer.parquet')
 
+# Funcion Developer
 def developer(desarrollador: str):
     # Filtrar los datos para el desarrollador dado
     desarrollador_data = df_desarrolladores[df_desarrolladores['publisher'] == desarrollador]
@@ -47,4 +48,40 @@ def developer(desarrollador: str):
         return {"error": "Desarrollador no encontrado"}
 
 
-#
+#def UserForGenre( genero : str ): Debe devolver el usuario que acumula más horas jugadas 
+# para el género dado y una lista de la acumulación de horas jugadas por año de lanzamiento.
+
+# Cargar el Dataset
+df_combined = pd.read_parquet('./Datasets/def_userdata.parquet')
+
+# Función Userdata
+def userdata(User_id: str):
+    # Filtrar el DataFrame combinado por el User_id dado
+    user_data = df_combined[df_combined['user_id'] == User_id].copy()  # Copiamos el DataFrame para evitar SettingWithCopyWarning
+    
+    if user_data.empty:
+        return {"error": "Usuario no encontrado"}
+    
+    # Filtrar los precios que sean de tipo float
+    user_data_float_prices = user_data[user_data['price'].apply(lambda x: isinstance(x, float))]
+    
+    # Calcular la cantidad de dinero gastado por el usuario
+    total_money_spent = user_data_float_prices['price'].sum()
+    
+    # Calcular el porcentaje de recomendación positiva
+    num_positive_recommendations = user_data[user_data['recommend']].shape[0]  # Usamos directamente valores booleanos
+    
+    if len(user_data) > 0:
+        recommend_percentage = (num_positive_recommendations / len(user_data)) * 100
+    else:
+        recommend_percentage = 0
+    
+    # Calcular la cantidad de items del usuario
+    total_items = len(user_data)
+    
+    return {
+        "Usuario": User_id,
+        "Dinero gastado": f"${total_money_spent:.2f}",
+        "% de recomendación positiva": f"{recommend_percentage:.2f}%",
+        "Cantidad de items": total_items
+    }
